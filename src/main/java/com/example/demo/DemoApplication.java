09db6278c91ff54058d0a50de6ff6ae502bdff72
Shahbaz <jocnud@gmail.com>
2017-07-29 11:52:17 +0530

    initial checkin

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,7 +13,7 @@ public class DemoApplication {
 	}
 }
 
-@Controller
+@RestController
 class Test {
 
 	@GetMapping
